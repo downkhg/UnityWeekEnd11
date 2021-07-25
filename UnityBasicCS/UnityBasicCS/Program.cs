ec834c49c8005ec847028fafc02e6d4559240fde
downkhg <downkhg@gmail.com>
2021-07-25 13:21:52 +0900

    첫수업

---
+++ UnityBasicCS/UnityBasicCS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnityBasicCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//구문
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("KHG");
+            Console.WriteLine("Add:" + Add(10, 20));
+            ValMain();
+        }//구문끝
+
+        static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        static void ValMain()
+        {
+            //내용
+        }
+    }
+}
