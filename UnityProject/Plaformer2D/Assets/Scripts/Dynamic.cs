de36bacc22bbf606a13262eaa4efeac6a2b93939
downkhg <downkhg@gmail.com>
2021-08-15 17:51:16 +0900

    플랫폼,아이템추가

--- UnityProject/Plaformer2D/Assets/Scripts/Dynamic.cs
+++ UnityProject/Plaformer2D/Assets/Scripts/Dynamic.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Dynamic : MonoBehaviour
 {
+    public float JumpPower;
+    public bool isGround;
+    public int Score;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -17,10 +20,42 @@ public class Dynamic : MonoBehaviour
             transform.position += Vector3.right * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position += Vector3.left * Time.deltaTime;
-        //위아래로 움직여보기
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += Vector3.up * Time.deltaTime;
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position += Vector3.down * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGround)
+            {
+                Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(Vector3.up * JumpPower);
+            }
+        }
+    }
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 20), "Score:" + Score);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "cherry")
+        {
+            Score += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            isGround = true;
+        }
+        Debug.Log("OnCollisionEnter2D:" + collision.gameObject.name);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            isGround = false;
+            Debug.Log("OnCollisionExit2D:" + collision.gameObject.name);
+        }
     }
 }
