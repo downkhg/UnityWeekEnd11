a67868990b1b4eac8300879d35177def8808a7fd
downkhg <downkhg@gmail.com>
2021-10-10 17:52:02 +0900

    무적모드 추가

--- UnityProject/Plaformer2D/Assets/Scripts/Opossum.cs
+++ UnityProject/Plaformer2D/Assets/Scripts/Opossum.cs
@@ -11,22 +11,44 @@ public class Opossum : MonoBehaviour
         
     }
 
+
+    private void FixedUpdate()
+    {
+        BoxCollider2D boxCollider = this.GetComponent<BoxCollider2D>();
+        Vector2 vPos = this.transform.position;
+        int nLayer = 1 << LayerMask.NameToLayer("Player");
+        Collider2D collider = Physics2D.OverlapBox(vPos + boxCollider.offset, boxCollider.size, 0, nLayer);
+
+        if(collider)
+        {
+            SuperMode superMode = collider.GetComponent<SuperMode>();
+            if (superMode && superMode.isUse == false)
+            {
+                Player monster = this.GetComponent<Player>();
+                Player player = collider.gameObject.GetComponent<Player>();
+                monster.Attack(player);
+                superMode.On();
+            }      
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.left * Speed * Time.deltaTime;
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            //Destroy(collision.gameObject); //플레이어 삭제
-            ////결과는 다를수있음. 어쨌든 없는 대상을 복제하는 행위가 올바른 방법이라고 할수없음.
-            //Instantiate(collision.gameObject); //플레이어 생성 -> 몬스터생성
-            Player monster = this.GetComponent<Player>();
-            Player player = collision.gameObject.GetComponent<Player>();
-            monster.Attack(player);
-        }
-    }
+    //몬스터와 플레이어가 충돌되어 데미지를 줄경우 정상적이지않은 결과를 가져온다.
+    //그러므로, 몬스터와 플레이어는 콜라이더를 이용하여 충돌체크가 불가능해진다.
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        //Destroy(collision.gameObject); //플레이어 삭제
+    //        ////결과는 다를수있음. 어쨌든 없는 대상을 복제하는 행위가 올바른 방법이라고 할수없음.
+    //        //Instantiate(collision.gameObject); //플레이어 생성 -> 몬스터생성
+    //        Player monster = this.GetComponent<Player>();
+    //        Player player = collision.gameObject.GetComponent<Player>();
+    //        monster.Attack(player);
+    //    }
+    //}
 }
---
+++ UnityProject/Plaformer2D/Assets/Scripts/SuperMode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperMode : MonoBehaviour
+{
+    public float Time;
+    public bool isUse;
+
+    IEnumerator ProcessTimmer()
+    {
+        isUse = true;
+        yield return new WaitForSeconds(Time);
+        GetComponent<SpriteRenderer>().color = Color.white;
+        isUse = false;
+    }
+
+    public void On()
+    {
+        StartCoroutine(ProcessTimmer());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUse)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+            Color color = spriteRenderer.color;
+            if (color.a == 1) color.a = 0;
+            else color.a = 1;
+            spriteRenderer.color = color;
+        }
+    }
+}
